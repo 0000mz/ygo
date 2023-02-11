5f4ea5a523af6700d497763badf12403de987a2e
Abdul-Muiz Yusuff <muiz.dev@gmail.com>
2023-02-10 18:11:05 -0800

    Updated install script to produce a packed binary

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+rm ygo
 rm -rf dist > /dev/null
 rm -rf build > /dev/null
 rm main.spec > /dev/null
 
 pip install -r requirements.txt
-pyinstaller main.py
-cp dist/main/main ygo
+pyinstaller -F main.py
+cp dist/main ygo
 
 rm -rf dist > /dev/null
 rm -rf build > /dev/null
